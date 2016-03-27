6c3e41afeb86e8c3440d96a2fb28cb84ae5a2e7f
Okimoka <wonthewarp@googlemail.com>
2016-03-27 22:11:35 +0200

    index.js

--- index.js
+++ index.js
@@ -8,13 +8,22 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 console.log('a user connected');
-socket.on('xy', function(msg) {
-console.log(msg);
-io.emit('pos', msg);
-});
+
+games = ["TF2", "Terraria", "Minecraft", "CSGO"];
+
+
+for (var i = 0; i < games.length; i++) {
+	eval('socket.on("'+games[i]+'", function(vc) {io.emit("'+games[i]+'", vc);})')
+};
+
+
+socket.on("spun", function(vs) {io.emit("adaptVs", vs);})
+socket.on("freshCharts", function(fresh){io.emit("freshdumbles", fresh)})
+socket.on("realthing", function(stronk){io.emit("actuallySpun", stronk)})
+
 
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 5000, function(){
+  console.log('listening on *:5000');
 });
